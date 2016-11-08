d040e224517307b6c843abbba700084571caebd5
dongshujin <dongshujin.beans@gmail.com>
2016-11-09 00:43:29 +0800

    Change: go practice

--- src/dongshujin.com/web/hello.go
+++ src/dongshujin.com/web/hello.go
@@ -5,7 +5,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
+	"time"
 )
 
 func sayHello(writer http.ResponseWriter, request *http.Request) {
@@ -27,12 +29,35 @@ func sayHello(writer http.ResponseWriter, request *http.Request) {
 func login(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Method: ", request.Method)
 	if "GET" == request.Method {
-		t, _ := template.ParseFiles("web/login.gtpl")
+		theDate := time.Date(2016, time.November, 10, 23, 0, 0, 0, time.UTC)
+		fmt.Printf("Date is : %s\n", theDate.Local())
+		t, _ := template.ParseFiles("src/dongshujin.com/web/login.gtpl")
 		t.Execute(writer, nil)
 	} else {
+		// Way NO.1
+		// 1) 不需要显式的调用 requst.ParseForm(), 会自动的调用
+		// 2) request.FormValue("param")的方式，只会返回同名参数中的第一个
+		//username := request.FormValue("username")
+		//password := request.FormValue("password")
+		//fmt.Println("Username: ", username, " password: ", password)
+
+		// Way NO.2
 		request.ParseForm()
 		fmt.Println("Username: ", request.Form["username"])
 		fmt.Println("Password: ", request.Form["password"])
+
+		if len(request.Form["username"][0]) == 0 {
+			fmt.Println("Username is blank")
+		}
+
+		if matched, _ := regexp.MatchString("^[\\x{4e00}-\\x{9fa5}]+$", request.Form["username"][0]); !matched {
+			fmt.Println("Username Must be Hanzi")
+		}
+
+		if matched, _ := regexp.MatchString("^([\\w\\.\\_]{2,10})@(\\w{1.}).([a-z]{2,4})$", request.Form["email"][0]); !matched {
+			fmt.Println("Email formate is wrong")
+		}
+
 	}
 }
 
--- src/dongshujin.com/web/login.gtpl
+++ src/dongshujin.com/web/login.gtpl
@@ -4,6 +4,8 @@
 </head>
 <body>
     <form action="http://localhost:9999/login" method="post">
+        邮箱： <input type="text" name="email">
+
         用户名：<input type="text" name="username">
         密码：<input type="password" name="password">
         <input type="submit" value="登陆">
