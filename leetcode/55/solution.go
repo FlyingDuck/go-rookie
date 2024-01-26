8931cdddf79300ac4a75afdd5179a0abda2f4d11
dongshujin <dongshujin.beans@gmail.com>
2024-01-26 09:04:16 +0800

    leetcode: 274 continue

--- leetcode/55/solution.go
+++ leetcode/55/solution.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
 55. 跳跃游戏
 
@@ -12,9 +16,35 @@ package main
 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
 */
 func main() {
-
+	nums := []int{2, 3, 1, 1, 4}
+	//nums := []int{3, 2, 1, 0, 4}
+	result := canJump(nums)
+	fmt.Println(result)
 }
 
+// 直接遍历所有点，如果当前点比前面所有点可以达到的点的最大距离还要大就返回false，如果能遍历到最后一个点，说明最后一个点可以到达，返回true
 func canJump(nums []int) bool {
-
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if i > k {
+			return false
+		}
+		k = max(i+nums[i], k)
+	}
+	return true
 }
+
+//func canJump(nums []int) bool {
+//	dp := make([]int, len(nums))
+//	dp[0] = 1
+//
+//	for i := 1; i < len(nums); i++ {
+//		for j := 0; j < i; j++ {
+//			if nums[j]+j >= i {
+//				dp[i] += dp[j]
+//			}
+//		}
+//	}
+//
+//	return dp[len(nums)-1] > 0
+//}
